0c8b0734e91ea610eade6a801e4c5dd5f2b884e8
alexwhitelockw <alexwhitelockw@gmail.com>
2022-05-30 18:04:08 +1000

    Created full colour palette output

--- scripts/create_colour_palette.py
+++ scripts/create_colour_palette.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-# Read Colour Palette Files
+# Read and Merge Colour Palette Files
 team_names = pd.read_csv("data/colour_palette/team_names.csv")
 team_colour_palette = pd.read_csv("data/colour_palette/team_colour_palette.csv")
 
@@ -9,3 +9,24 @@ team_colour_palette = team_names.merge(
         on="team_number"
     )
 
+# Correct colour names to hex values
+
+team_colour_palette.loc[:, "colour"] = team_colour_palette["colour"].str.lower()
+
+team_colour_palette.loc[team_colour_palette["colour"] == "maroon", "colour"] = "#800000"
+team_colour_palette.loc[team_colour_palette["colour"] == "gold", "colour"] = "#FFD700"
+team_colour_palette.loc[team_colour_palette["colour"] == "white", "colour"] = "#FFFFFF"
+team_colour_palette.loc[team_colour_palette["colour"] == "skyblue", "colour"] = "#87CEEB"
+team_colour_palette.loc[team_colour_palette["colour"] == "black", "colour"] = "#000000"
+team_colour_palette.loc[team_colour_palette["colour"] == "dodgerblue", "colour"] = "#1E90FF"
+team_colour_palette.loc[team_colour_palette["colour"] == "red", "colour"] = "#FF0000"
+team_colour_palette.loc[team_colour_palette["colour"] == "navy", "colour"] = "#1E90FF"
+team_colour_palette.loc[team_colour_palette["colour"] == "green", "colour"] = "#008000"
+team_colour_palette.loc[team_colour_palette["colour"] == "yellow", "colour"] = "#FFFF00"
+team_colour_palette.loc[team_colour_palette["colour"] == "silver", "colour"] = "#C0C0C0"
+
+team_colour_palette.loc[:, "colour"] = team_colour_palette["colour"].str.upper()
+
+# Output Palette to CSV
+
+team_colour_palette[["team_name", "colour_number", "colour"]].to_csv("data/colour_palette/full_nrl_colour_palette.csv", index=False)
